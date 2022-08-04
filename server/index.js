d35f1619c174b471d072c6e84a9ef347740f34cd
Ciano Dev <lorencianodramayo@gmail.com>
2022-08-04 20:45:34 +0800

    initial commit

---
+++ server/index.js
@@ -0,0 +1,52 @@
+// Import npm packages
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
+const path = require("path");
+
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 8080; // Step 1
+
+const routes = require("./routes/api");
+
+// Step 2
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose is connected!!!!");
+});
+
+// Data parsing
+app.use(express.json());
+app.use(
+  cors({
+    allowedHeaders: ["sessionId", "Content-Type"],
+    exposedHeaders: ["sessionId"],
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+  })
+);
+app.use(express.urlencoded({ extended: false }));
+
+// Step 3
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// HTTP request logger
+app.use(morgan("tiny"));
+app.use("/api", routes);
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
+
+app.listen(PORT, console.log(`Server is starting at ${PORT}`));
